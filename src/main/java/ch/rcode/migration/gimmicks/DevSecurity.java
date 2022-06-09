06bfec1dd2761f04d37250cb876de7fc615bdf77
Rafal Gorczynski <root@pc-rig.localdomain>
2022-06-09 19:31:44 +0200

    migrated with gimmick

--- src/main/java/ch/rcode/migration/gimmicks/DevSecurity.java
+++ src/main/java/ch/rcode/migration/gimmicks/DevSecurity.java
@@ -2,11 +2,12 @@ package ch.rcode.migration.gimmicks;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DevSecurity {
@@ -16,29 +17,27 @@ public class DevSecurity {
 
         @Configuration
         @Order(Ordered.HIGHEST_PRECEDENCE)
-        public class InnerConfiguration extends WebSecurityConfigurerAdapter {
+        public class InnerConfiguration {
 
             private final Logger log = LoggerFactory.getLogger(InnerConfiguration.class);
 
-            @Override
-            protected void configure(HttpSecurity http) throws Exception {
-                super.configure(http);
-                log.info("configure executed in InnerConfiguration");
-
+            @Bean
+            public SecurityFilterChain innerChain(HttpSecurity http) throws Exception {
+                log.info("Bean innerChain built in InnerConfiguration");
+                return http.build();
             }
         }
     }
 
     @Configuration
-    public class SecondConfiguration extends WebSecurityConfigurerAdapter {
+    public class SecondConfiguration {
 
         private final Logger log = LoggerFactory.getLogger(SecondConfiguration.class);
 
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            super.configure(http);
-            log.info("configure executed in SecondConfiguration");
-
+        @Bean
+        public SecurityFilterChain secondChain(HttpSecurity http) throws Exception {
+            log.info("Bean secondChain built in SecondConfiguration");
+            return http.build();
         }
     }
 }
